02b276fa97545441125e868229959436fe99a695
NuwanJ <nuwanjaliyagoda@gmail.com>
2020-10-20 16:24:05 +0530

    .

--- src/js/app/managers/mqttClient.js
+++ src/js/app/managers/mqttClient.js
@@ -39,6 +39,8 @@ export default class MQTTClient {
 
             window.robot = this.robot;
 
+            console.log(this.client);
+
             this.client.onMessageArrived = this.onMessageArrived;
             this.client.onConnectionLost = this.onConnectionLost;
          }
