05d652ee909b25b687c7260c95fadd01487c4bbc
NuwanJ <nuwanjaliyagoda@gmail.com>
2020-10-26 14:59:33 +0530

    log scheme updated

--- src/js/app/components/robot.js
+++ src/js/app/components/robot.js
@@ -8,91 +8,103 @@ const TOPIC_INFO = 'v1/localization/info';
 const TOPIC_CREATE = 'v1/gui/create';
 
 export default class Robot {
-   constructor(scene) {
-      this.scene = scene;
-   }
+    constructor(scene) {
+        this.scene = scene;
+    }
 
-   changeColor(id, R, G, B, ambient, callback) {
-      var r = this.scene.getObjectByName("id_" + id);
-      if (r != undefined) {
-         r.material.color.setRGB(R/256, G/256, B/265);
-         if (callback != null) callback('success');
-      } else {
-         if (callback != null) callback('undefined');
-      }
-
-      return r;
-   }
-
-   create(id, x, y, heading, callback) {
-      var r = this.scene.getObjectByName("id_" + id);
-      if (r == undefined) {
-         // Create only if not exists
-         var loader = new STLLoader();
-         loader.load('./assets/models/model.stl', function (geometry, scene) {
-            var material = new THREE.MeshStandardMaterial({ color: 0x9e9e9e });
-
-            var r = new THREE.Mesh(geometry, material);
-            r.receiveShadow = true;
-            r.name = "id_" + id;
-            r.position.set(x, 0, y);
-            r.rotation.y = heading * THREE.Math.DEG2RAD;
-            window.scene.add(r);
-
-            // Callback function
+    changeColor(id, R, G, B, ambient, callback) {
+        var r = this.scene.getObjectByName("id_" + id);
+        if (r != undefined) {
+            r.material.color.setRGB(R/256, G/256, B/265);
             if (callback != null) callback('success');
-         });
-      } else {
-         if (callback != null) callback('already defined');
-      }
-      return r;
-   }
+        } else {
+            if (callback != null) callback('undefined');
+        }
 
-   move(id, x, y, heading, callback) {
-      var r = this.scene.getObjectByName("id_" + id);
-      if (r != undefined) {
-         const newHeading = heading * THREE.Math.DEG2RAD;
-         var position = { x: r.position.x, y: r.position.z, heading: r.rotation.y };
+        return r;
+    }
 
-         const speed = 10;
-         const distance = Math.sqrt(Math.pow(x - position.x, 2) + Math.pow(y - position.y, 2));
+    create(id, x, y, heading, callback) {
+        var r = this.scene.getObjectByName("id_" + id);
+        if (r == undefined) {
+            // Create only if not exists
+            var loader = new STLLoader();
+            loader.load('./assets/models/model.stl', function (geometry, scene) {
+                var material = new THREE.MeshStandardMaterial({ color: 0x5877d2 });
 
-         // TODO: If distance is 0, need to handle only the rotation
+                var r = new THREE.Mesh(geometry, material);
+                r.receiveShadow = true;
+                r.name = "id_" + id;
+                r.position.set(x, 0, y);
+                r.rotation.y = heading * THREE.Math.DEG2RAD;
+                window.scene.add(r);
 
-         if (distance != 0) {
-            var tween = new TWEEN.Tween(position).to({ x: x, y: y, heading: newHeading }, 1000 * (distance / speed))
-            /*.easing(TWEEN.Easing.Quartic.InOut)*/
-            .onUpdate(function () {
-               r.position.x = position.x;
-               r.position.z = position.y;
-               r.rotation.y = position.heading;
+                console.log('Created:', id);
 
-            }).onComplete(() => {
-               if (callback != null) callback('success');
+                // Callback function
+                if (callback != null) callback('success');
+            });
+        } else {
+            if (callback != null) callback('already defined');
+        }
+        return r;
+    }
 
-            }).delay(50).start();
-         } else {
-            // No move, only the rotation
-            r.rotation.y = newHeading;
-         }
-         return r;
+    exists(id){
+        var r = this.scene.getObjectByName("id_" + id);
+        return r;
+    }
 
-      } else {
-         if (callback != null) callback('undefined');
-      }
-   }
+    move(id, x, y, heading, callback) {
+        var r = this.scene.getObjectByName("id_" + id);
+        if (r != undefined) {
+            const newHeading = heading * THREE.Math.DEG2RAD;
+            var position = { x: r.position.x, y: r.position.z, heading: r.rotation.y };
 
-   get_coordinates(id) {
-      var r = this.scene.getObjectByName("id_" + id);
-      if (r != undefined) {
-         console.log(`${r.position.x},${r.position.y},${r.position.z}`);
-         return r;
-      } else {
-         return null;
-      }
-   }
+            x = Math.round(x*10)/10;
+            y = Math.round(y*10)/10;
+            heading = Math.round(heading*10)/10;
 
-   update() {
-      TWEEN.update();
-   }
+            const speed = 10;
+            const distance = 10;//Math.sqrt(Math.pow(x - position.x, 2) + Math.pow(y - position.y, 2));
+
+            // TODO: If distance is 0, need to handle only the rotation
+
+            if (distance != 0) {
+                var tween = new TWEEN.Tween(position).to({ x: x, y: y, heading: newHeading }, 1000 * (distance / speed))
+                /*.easing(TWEEN.Easing.Quartic.InOut)*/
+                .onUpdate(function () {
+                    r.position.x = position.x;
+                    r.position.z = position.y;
+                    r.rotation.y = position.heading;
+
+                }).onComplete(() => {
+                    console.log('Move:',id,'x:',x,'y:',y,'heading',heading);
+                    if (callback != null) callback('success');
+
+                }).delay(50).start();
+            } else {
+                // No move, only the rotation
+                r.rotation.y = newHeading;
+            }
+            return r;
+
+        } else {
+            if (callback != null) callback('undefined');
+        }
+    }
+
+    get_coordinates(id) {
+        var r = this.scene.getObjectByName("id_" + id);
+        if (r != undefined) {
+            console.log(`${r.position.x},${r.position.y},${r.position.z}`);
+            return r;
+        } else {
+            return null;
+        }
+    }
+
+    update() {
+        TWEEN.update();
+    }
 }
--- src/js/app/managers/mqttClient.js
+++ src/js/app/managers/mqttClient.js
@@ -13,84 +13,91 @@ const TOPIC_CHANGE_COLOR = 'v1/sensor/color';
 
 export default class MQTTClient {
 
-   constructor(scene, robot) {
+    constructor(scene, robot) {
 
-      this.scene = scene;
-      this.robot = robot;
+        this.scene = scene;
+        this.robot = robot;
 
-      const client_id = 'client_' + Math.random().toString(36).substring(2, 15);
-      this.client = new MQTT.Client(mqtt_server, mqtt_port, "", client_id);
+        const client_id = 'client_' + Math.random().toString(36).substring(2, 15);
+        this.client = new MQTT.Client(mqtt_server, mqtt_port, "", client_id);
 
-      this.client.connect({
-         userName: "swarm_user",
-         password: "swarm_usere15",
-         reconnect: true,
-         onSuccess: () => {
-            console.log('MQTT: connected');
+        this.client.connect({
+            userName: "swarm_user",
+            password: "swarm_usere15",
+            reconnect: true,
+            onSuccess: () => {
+                console.log('MQTT: connected');
 
-            // Subscribe to topics
-            this.client.subscribe(TOPIC_INFO);
-            this.client.subscribe(TOPIC_CREATE);
-            this.client.subscribe(TOPIC_CHANGE_COLOR);
+                // Subscribe to topics
+                this.client.subscribe(TOPIC_INFO);
+                this.client.subscribe(TOPIC_CREATE);
+                this.client.subscribe(TOPIC_CHANGE_COLOR);
 
-            window.robot = this.robot;
-            this.client.onMessageArrived = this.onMessageArrived;
-            this.client.onConnectionLost = this.onConnectionLost;
-         }
-      });
-   }
+                window.robot = this.robot;
+                this.client.onMessageArrived = this.onMessageArrived;
+                this.client.onConnectionLost = this.onConnectionLost;
+            }
+        });
+    }
 
-   onConnectionLost(responseObject) {
-      if (responseObject.errorCode !== 0) {
-         console.log("MQTT: onConnectionLost:" + responseObject.errorMessage);
-         console.log('MQTT: reconnecting');
-      }
-   }
+    onConnectionLost(responseObject) {
+        if (responseObject.errorCode !== 0) {
+            console.log("MQTT: onConnectionLost:" + responseObject.errorMessage);
+            console.log('MQTT: reconnecting');
+        }
+    }
 
-   onMessageArrived(packet) {
-      const msg = packet.payloadString.trim();
-      const topic = packet.destinationName;
-      console.log('MQTT: ' + topic + ' > ' + msg);
+    onMessageArrived(packet) {
+        const msg = packet.payloadString.trim();
+        const topic = packet.destinationName;
 
-      if (topic == TOPIC_CREATE) {
-         try {
-            var data = JSON.parse(msg);
-            window.robot.create(data.id, data.x, data.y, data.heading)
-         } catch (e) {
-            console.error(e);
-         }
+        if (topic == TOPIC_CREATE) {
+            //console.log('MQTT: ' + topic + ' > ' + msg);
 
-      } else if (topic == TOPIC_INFO) {
-         try {
-            var data = JSON.parse(msg);
+            try {
+                var data = JSON.parse(msg);
+                window.robot.create(data.id, data.x, data.y, data.heading)
+            } catch (e) {
+                console.error(e);
+            }
 
-            Object.entries(data).forEach(entry => {
-               // Update each robot
-               console.log(entry[1]);
-               const r = entry[1];
-               window.robot.move(r.id, r.x, r.y, r.heading);
-            });
-         } catch (e) {
-            console.error(e);
-         }
-      } else if (topic == TOPIC_CHANGE_COLOR) {
-         try {
-            var data = JSON.parse(msg);
-            window.robot.changeColor(data.id, data.R, data.G, data.B, data.ambient);
-            //console.log('R: '+data.R+ ' G: '+data.G+ ' B: '+data.B);
-         } catch (e) {
-            console.error(e);
-         }
-      }
-   }
+        } else if (topic == TOPIC_INFO) {
+            //console.log('MQTT: ' + topic + ' > ' + msg);
+            try {
+                var data = JSON.parse(msg);
 
-   publish(topic, message, callback) {
-      var payload = new MQTT.Message(message);
-      payload.destinationName = topic;
-      this.client.send(payload);
-      console.log('MQTT: published');
+                Object.entries(data).forEach(entry => {
+                    // Update each robot
+                    //console.log(entry[1]);
+                    const r = entry[1];
 
-      if (callback != null) callback();
-   }
+                    if(window.robot.exists(r.id)==undefined){
+                        window.robot.create(r.id, r.x, r.y, r.heading);
+                    }else{
+                        window.robot.move(r.id, r.x, r.y, r.heading);
+                    }
+                });
+            } catch (e) {
+                console.error(e);
+            }
+        } else if (topic == TOPIC_CHANGE_COLOR) {
+            try {
+                var data = JSON.parse(msg);
+                window.robot.changeColor(data.id, data.R, data.G, data.B, data.ambient);
+                //console.log('R: '+data.R+ ' G: '+data.G+ ' B: '+data.B);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    }
+
+    publish(topic, message, callback) {
+        var payload = new MQTT.Message(message);
+        payload.destinationName = topic;
+        this.client.send(payload);
+        console.log('MQTT: published');
+
+        if (callback != null) callback();
+    }
 
 }
