59004907cb106038a4115ac885bb8f69cde01417
Ganindu Dhananja <ganindudananja@gmail.com>
2021-02-13 18:01:07 +0530

    toggleReality feature

--- src/js/app/managers/datGUI.js
+++ src/js/app/managers/datGUI.js
@@ -51,8 +51,6 @@ export default class DatGUI {
         /* Reality Folder */
         const realityFolder = this.gui.addFolder('Reality');
 
-        // TODO: mixed reality = p + v
-        // No need of additional toggle
         realityFolder
             .add(realities, 'physical')
             .name('Physical Reality')
@@ -87,36 +85,20 @@ export default class DatGUI {
         }
     }
 
-    toggleRobotsByRealm(objects, type) {
-        if (Array.isArray(objects) && type !== undefined && type !== '') {
-            for (let variable of objects) {
-                // TODO:
-            }
-        }
-    }
-
     toggleReality(reality, selected) {
-        // TODO: refer Config.mixedReality for actual changes
         // by default visualizer will intercept all the communication coming to the channel regardless of the reality.
-        // this control panel will only toggle the visibility of objects in the selected realities.
+        // this control panel will only toggle the 'visibility' of objects in the selected realities.
+        window.selectedRealities = realities;
         const objects = scene.children;
         Object.entries(objects).forEach((obj) => {
             const name = obj[1]['name'];
             const reality = obj[1]['reality'];
-
-            if (reality != undefined) {
-                // TODO: hide by making opacity = 0 for non-matched obstacles
-                // or any other solution
-                // obj[1].material.opacity = 0;
+            if (reality !== undefined && reality === 'P') {
+                obj[1].visible = realities.physical;
+            } else if (reality !== undefined && reality === 'V') {
+                obj[1].visible = realities.virtual;
             }
         });
-
-        window.selectedReality = selected;
-        for (let attrib in realities) {
-            realities[attrib] = false;
-        }
-        realities[reality] = true;
-        console.log(window.selectedReality, realities);
     }
 
     show() {
