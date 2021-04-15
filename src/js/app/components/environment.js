fd2877a9d8f40bfddee2873b38a9a408611f54c8
NuwanJ <nuwanjaliyagoda@gmail.com>
2021-04-15 13:00:50 +0530

    URL redirection and JWT configuration completed.

--- src/js/app/components/environment.js
+++ src/js/app/components/environment.js
@@ -21,7 +21,7 @@ export default class Environment {
         scene.add(ground);
 
         // Grid
-        var grid = new THREE.GridHelper(Config.arena.size, Math.round(Config.arena.size/10), 0x000000, 0x5b5b5b);
+        var grid = new THREE.GridHelper(Config.arena.size, Math.round(Config.arena.size / 10), 0x000000, 0x5b5b5b);
         grid.rotation.x = -Math.PI / 2;
         grid.scale.set(scene_scale, scene_scale, scene_scale);
         grid.position.set(0, 0, 0);
